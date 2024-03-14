f30a1eab46743e65cbdf2fd55a5b09aaf3550ee3
Texmo2003 <jtgronn@gmail.com>
2024-03-14 19:41:46 +0100

    initialize project

---
+++ myportfolio/src/App.js
@@ -0,0 +1,23 @@
+import './components/css/styles.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './components/profile.js';
+import Header from './components/header.js';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ height: "100vh" }}>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Header header="Min profil"/>
+              <Profile />
+            </>
+          }/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
